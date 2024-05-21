b5f5d9827ff003e294f695f4dfebc5e32215ac67
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-05-21 15:29:36 +0000

    add a new column to contracted model

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -17,7 +17,8 @@ rename as (
         o_orderpriority as priority_code,
         o_clerk as clerk_name,
         o_shippriority as ship_priority,
-        o_comment as comment
+        o_comment as comment,
+        'test' as new_column
 
     from source
 
