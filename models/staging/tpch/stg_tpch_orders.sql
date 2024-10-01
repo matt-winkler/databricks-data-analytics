7c7982b26bc9e2df00099ef18c469d9ffe8d57e5
Matt Winkler <matt.winkler@fishtownanalytics.com>
2024-10-01 19:10:51 +0000

    fix foo bar column in stg_tpch_orders

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -17,8 +17,7 @@ rename as (
         o_orderpriority as priority_code,
         o_clerk as clerk_name,
         o_shippriority as ship_priority,
-        o_comment as comment,
-        'foo' as bar
+        o_comment as comment
 
     from source
 
